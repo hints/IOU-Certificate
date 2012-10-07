860ef01ce73f1bdc4f5a1e99b14d4c8c8ac455bb
Ricky Wong <rickwong@gmail.com>
2012-10-06 19:02:23 -0700

    aws update

--- server/ioucerts/ioucerts/settings.py
+++ server/ioucerts/ioucerts/settings.py
@@ -12,11 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/rickywong/Documents/IOU-Certificate/ioucerts.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'fastiou',
+        # /Users/rickywong/Documents/IOU-Certificate/ioucerts.db',                      # Or path to database file if using sqlite3.
+        'USER': 'fastiou',
+        'PASSWORD': 'fastiou1',
+        'HOST': 'ec2-204-236-167-47.us-west-1.compute.amazonaws.com',
+        'PORT': '22',
     }
 }
 
